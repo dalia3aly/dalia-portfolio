007d0464b2b7c31219abedf04f5c1f8bbfc9e227
Dalia Aly <137359389+daliamnhaly@users.noreply.github.com>
2024-03-17 23:18:17 +1100

    Added - Arch projects

--- js/projectDetails.js
+++ js/projectDetails.js
@@ -1,11 +1,77 @@
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
+//     const projectId = localStorage.getItem('selectedProjectId');
+//     if (!projectId) {
+//         console.error('No project ID found in local storage.');
+//         return;
+//     }
+
+//     fetch('../assets/data/webProjects.json') // Adjust the path as necessary
+//         .then(response => response.json())
+//         .then(projects => {
+//             const project = projects.find(p => p.id.toString() === projectId);
+//             if (project) {
+//                 document.title = project.appName;
+                
+//                 const detailsContainer = document.getElementById('project-details');
+//                 if (!detailsContainer) {
+//                     console.error('Project details container not found.');
+//                     return;
+//                 }
+                
+//                 detailsContainer.innerHTML = ''; // Clearing detailsContainer before adding new content
+
+//                 const titleElement = document.createElement('h2');
+//                 titleElement.textContent = project.appName;
+//                 detailsContainer.appendChild(titleElement);
+
+//                 const descriptionElement = document.createElement('p');
+//                 descriptionElement.textContent = project.description;
+//                 detailsContainer.appendChild(descriptionElement);
+
+//                 const techStackElement = document.createElement('p');
+//                 techStackElement.textContent = `Technologies used: ${project.techStack}`;
+//                 detailsContainer.appendChild(techStackElement);
+
+//                 const myRoleElement = document.createElement('p');
+//                 myRoleElement.textContent = `${project.myRole}`;
+//                 detailsContainer.appendChild(myRoleElement);
+                
+//                 // Create a new container for images with the 'project-details-grid' class
+//                 const gridContainer = document.createElement('div');
+//                 gridContainer.className = 'project-details-grid'; // Assigning the new class
+                
+//                 // Append the grid container to the detailsContainer
+//                 detailsContainer.appendChild(gridContainer);
+
+//                 // Creating and appending image elements to the gridContainer
+//                 project.images.forEach(imageSrc => {
+//                     const imgElement = document.createElement('img');
+//                     imgElement.src = imageSrc;
+//                     imgElement.alt = project.appName;
+//                     // Apply initial width, which will be overridden by CSS @media queries
+//                     imgElement.style.width = '100%'; 
+//                     gridContainer.appendChild(imgElement); // Now appending images to the gridContainer
+//                 });
+//             } else {
+//                 console.error('Project not found.');
+//             }
+//         })
+//         .catch(error => console.error('Error loading project details:', error));
+// });
+
+
+
+// dynamically use this reusable function instead, for both architecture & web development JSON files;
+function loadProjectDetails(jsonPath) {
     const projectId = localStorage.getItem('selectedProjectId');
-    if (!projectId) {
-        console.error('No project ID found in local storage.');
+    const jsonFilePath = localStorage.getItem('selectedJsonFilePath'); // Retrieve the JSON file path
+
+    if (!projectId || !jsonFilePath) {
+        console.error('No project ID or JSON file path found in local storage.');
         return;
     }
 
-    fetch('../assets/data/webProjects.json') // Adjust the path as necessary
+    fetch(jsonPath) // Use the jsonPath parameter
         .then(response => response.json())
         .then(projects => {
             const project = projects.find(p => p.id.toString() === projectId);
@@ -17,8 +83,8 @@ document.addEventListener("DOMContentLoaded", function() {
                     console.error('Project details container not found.');
                     return;
                 }
-                
-                detailsContainer.innerHTML = ''; // Clearing detailsContainer before adding new content
+
+                detailsContainer.innerHTML = '';
 
                 const titleElement = document.createElement('h2');
                 titleElement.textContent = project.appName;
@@ -31,26 +97,29 @@ document.addEventListener("DOMContentLoaded", function() {
                 const techStackElement = document.createElement('p');
                 techStackElement.textContent = `Technologies used: ${project.techStack}`;
                 detailsContainer.appendChild(techStackElement);
-                
-                // Create a new container for images with the 'project-details-grid' class
-                const gridContainer = document.createElement('div');
-                gridContainer.className = 'project-details-grid'; // Assigning the new class
-                
-                // Append the grid container to the detailsContainer
-                detailsContainer.appendChild(gridContainer);
 
-                // Creating and appending image elements to the gridContainer
-                project.images.forEach(imageSrc => {
+                const myRoleElement = document.createElement('p');
+                myRoleElement.textContent = `${project.myRole}`;
+                detailsContainer.appendChild(myRoleElement);
+
+                // a new container for images with the 'project-details-grid' class to maintain the grid layout
+                 const gridContainer = document.createElement('div');
+                 gridContainer.className = 'project-details-grid';
+
+                 detailsContainer.appendChild(gridContainer);
+
+                 project.images.forEach(imageSrc => {
                     const imgElement = document.createElement('img');
                     imgElement.src = imageSrc;
                     imgElement.alt = project.appName;
-                    // Apply initial width, which will be overridden by CSS @media queries
-                    imgElement.style.width = '100%'; 
-                    gridContainer.appendChild(imgElement); // Now appending images to the gridContainer
+                    gridContainer.appendChild(imgElement);  // append images to gridContainer instead of detailsContainer
                 });
             } else {
                 console.error('Project not found.');
             }
         })
         .catch(error => console.error('Error loading project details:', error));
-});
+}
+
+loadProjectDetails('../assets/data/webProjects.json');
+loadProjectDetails('../assets/data/archProjects.json');
--- js/script.js
+++ js/script.js
@@ -1,37 +1,88 @@
 // Event listener for the DOMContentLoaded event - this is fired when the HTML has been fully loaded and parsed
-document.addEventListener("DOMContentLoaded", function() {
-    const projectsGrid = document.querySelector('.projects-grid');
-    if (!projectsGrid) {
-        console.error('Error: .projects-grid element not found');
-        return;
-    }
+// document.addEventListener("DOMContentLoaded", function() {
+//     const projectsGrid = document.querySelector('.projects-grid');
+//     if (!projectsGrid) {
+//         console.error('Error: .projects-grid element not found');
+//         return;
+//     }
 
-    fetch('../assets/data/webProjects.json') 
+//     fetch('../assets/data/webProjects.json') 
+//       .then(response => {
+//         if (!response.ok) {
+//           // If the response is not OK (i.e. 404, 500, etc.), log the error to the console
+//             throw new Error(`HTTP error! status: ${response.status}`);
+//         }
+//         return response.json();
+//       })
+//       .then(projects => {
+//         projects.forEach(project => {
+//           const projectItem = document.createElement('div');
+//           projectItem.className = 'project-item';
+//           projectItem.innerHTML = `
+//             <a href="javascript:void(0);" onclick="openProjectDetails(${project.id})">
+//               <img src="${project.logo}" alt="${project.appName}" />
+//               <div class="project-info">
+//                 <h2>${project.appName}</h2>
+//               </div>
+//             </a>
+//           `;
+//           // Add the project item to the projects grid
+//           projectsGrid.appendChild(projectItem);
+//         });
+//       })
+//       .catch(error => console.error('Error loading projects:', error));
+// });
+
+function loadProjectsGrid(jsonPath, targetSectionId) {
+  // Use the targetSectionId to select the correct projects-grid within the specified section
+  const projectsGrid = document.querySelector(`${targetSectionId} .projects-grid`);
+  if (!projectsGrid) {
+      console.error(`Error: .projects-grid element not found in ${targetSectionId}`);
+      return;
+  }
+
+  projectsGrid.innerHTML = ''; // Clear the projects grid before loading new projects
+
+  fetch(jsonPath)
       .then(response => {
-        if (!response.ok) {
-          // If the response is not OK (i.e. 404, 500, etc.), log the error to the console
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
+          if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
       })
       .then(projects => {
-        projects.forEach(project => {
-          const projectItem = document.createElement('div');
-          projectItem.className = 'project-item';
-          projectItem.innerHTML = `
-            <a href="javascript:void(0);" onclick="openProjectDetails(${project.id})">
-              <img src="${project.logo}" alt="${project.appName}" />
-              <div class="project-info">
-                <h2>${project.appName}</h2>
-              </div>
-            </a>
-          `;
-          // Add the project item to the projects grid
-          projectsGrid.appendChild(projectItem);
-        });
+          projects.forEach(project => {
+              const projectItem = document.createElement('div');
+              projectItem.className = 'project-item';
+              projectItem.innerHTML = `
+                  <a href="javascript:void(0);" onclick="openProjectDetails('${project.id}')">
+                      <img src="${project.logo}" alt="${project.appName}" />
+                      <div class="project-info">
+                          <h2>${project.appName}</h2>
+                      </div>
+                  </a>
+              `;
+              projectsGrid.appendChild(projectItem);
+          });
       })
       .catch(error => console.error('Error loading projects:', error));
-});
+}
+
+
+loadProjectsGrid('./assets/data/webProjects.json', '#webProjects');
+loadProjectsGrid('./assets/data/archProjects.json', '#archProjects');
+
+
+function openProjectDetails(projectId, jsonFilePath) {
+  const projectDetails = {
+    projectId: projectId.toString(),
+    jsonFilePath: jsonFilePath
+  };
+
+  localStorage.setItem('selectedProjectDetails', JSON.stringify(projectDetails));
+
+  window.open('project-detail.html', '_blank');
+}
 
 document.addEventListener('DOMContentLoaded', function() {
   const projectId = localStorage.getItem('selectedProjectId');
@@ -43,8 +94,9 @@ document.addEventListener('DOMContentLoaded', function() {
     .catch(error => console.error('Error loading project details:', error));
 });
 
-function openProjectDetails(projectId) {
+function openProjectDetails(projectId, jsonFilePath) {
   localStorage.setItem('selectedProjectId', projectId.toString());
+  localStorage.setItem('selectedJsonFilePath', jsonFilePath);
 
   window.open('project-detail.html', '_blank');
 }
